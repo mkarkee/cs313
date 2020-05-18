43ff20750ad190c2b94f4b79cacb1240d118d332
mkarkee <ermkarki@gmail.com>
2020-05-17 23:44:25 -0500

    css

--- web/shoppingcart/cart.php
+++ web/shoppingcart/cart.php
@@ -7,10 +7,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="checkoutStyle.css">
     <title>Document</title>
 </head>
 <body>
-    <table>
+    <table id="content">
             <?php 
                 foreach($_SESSION as $product) {
                     echo "<tr><td>";
@@ -23,7 +24,7 @@
             ?>
         </tr>
     </table>
-    <a href="browse.php">Continue Shopping  </a>
+    <a href="browse.php" id="button1">Continue Shopping  </a>
     <a href="checkout.php">Checkout</a>
 </body>
 </html>
