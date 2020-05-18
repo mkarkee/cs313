402e0e6e5bb02a2465810710baadeda5f3602b80
mkarkee <ermkarki@gmail.com>
2020-05-17 22:51:35 -0500

    a

--- web/shoppingcart/session.php
+++ web/shoppingcart/session.php
@@ -3,10 +3,10 @@
     session_start();
     $name = $_POST['name'];
     $price = $_POST['price'];
-    $quantity = $_POST['qty'];
 
     $product = array($name,$price, $quantity);
     $_SESSION[$name] = $product;
+    
 
     header('location:browse.php');
 
