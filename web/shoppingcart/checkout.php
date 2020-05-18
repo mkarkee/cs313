0e77387e979d5b939c057fef89161023d596a0a8
mkarkee <ermkarki@gmail.com>
2020-05-17 23:23:42 -0500

    Aa

--- web/shoppingcart/checkout.php
+++ web/shoppingcart/checkout.php
@@ -19,20 +19,6 @@
     <input type="text" placeholder="Phone Numer" name="phone_number" maxlength="12" size="12" onchange="validPhoneNumber(this)" required>
     <span id="phoneError"></span>
 
-
-
-div id="cards">
-        <input type="radio" name="card" value ="Visa" required><label>Visa</label><br>
-        <input type="radio" name="card" value="Mastercard"><label>Mastercard</label><br>
-        <input type="radio" name="card" value="American Express"><label>American Express</label>
-    </div>
-    <br>
-    <input type="text" name="credit_card" placeholder="Credit Card #" maxlength="20" size="20" onblur= "isCreditValid(this)" required>
-    <span id="CreditError"></span>
-    <br>
-    <input type="text" name="exp_date" placeholder="Expiration Date" onblur="isExpValid(this)" required>
-    <span id="expError"></span>
-    
     <br>
     <div>
         <input type="submit" value="Purchase" name="validate" />
--- web/shoppingcart/script.js
+++ web/shoppingcart/script.js
@@ -1,8 +1,6 @@
 var firstNameValid = true;
 var lastNameValid = true;
 var phoneNumberValid = true;
-var creditValid = true;
-var expValid = true;
 
 function validFirstName(el){
     var pattern = /[a-z]/ig;
@@ -48,53 +46,11 @@ function validPhoneNumber(el){
     }
 
 }
-function isCreditValid(el){
-    var one = /^\d{16}$/;
-    var two = /^\d{4} \d{4} \d{4} \d{4}$/;
-    var three = /^\d{4}\-\d{4}\-\d{4}\-\d{4}$/;
-    var str = el.value;
 
-    var isPatternAValid = one.test(str);
-    var isPatternBValid = two.test(str);
-    var isPatternCValid = three.test(str);
 
-    if(isPatternAValid || isPatternBValid || isPatternCValid){
-        document.getElementById('CreditError').innerHTML = "";
-        creditValid = true;
-    }
-    else{
-        document.getElementById('CreditError').innerHTML = "Error: Credit card should be in format: XXXX XXXX XXXX XXXX";
-        creditValid = false;
-    }
-
-}
-function isExpValid(el){
-    var pattern = /^[0-1]\d\/([0-2]\d|3[0-1])$/;
-    var str = el.value;
-
-    var isValid = pattern.test(str);
-    if(isValid){
-        document.getElementById('expError').innerHTML = "";
-        expValid = true;
-    }
-    else{
-      
-        document.getElementById('expError').innerHTML = "Error: Expiration date should be in format: MM/YY";
-        expValid = false;
-
-    }
-}
 
 
 function validAll() {
-
-    if(!expValid) {
-        document.getElementsByName('exp_date')[0].focus();
-    }
-
-    if(!creditValid) {
-        document.getElementsByName('credit_card')[0].focus();
-    }
 
     if(!phoneNumberValid) {
         document.getElementsByName('phone_number')[0].focus();
@@ -108,33 +64,11 @@ function validAll() {
         document.getElementsByName('first_name')[0].focus();
     }
 
-    if(expValid && creditValid && phoneNumberValid && lastNameValid && firstNameValid) {
+    if(phoneNumberValid && lastNameValid && firstNameValid) {
         return true;
     }
     else {
         return false;
     }
 
-}
-function totalCost(){
-    var x = document.getElementsByClassName('check');
-    var y = new Array(4);
-
-
-    y[0] = document.getElementById('watchOne').innerHTML;
-    y[1] = document.getElementById('watchTwo').innerHTML;
-    y[2] = document.getElementById('watchThree').innerHTML;
-    y[3] = document.getElementById('watchFour').innerHTML;
-
-
-    var total = 0.00;
-    for(var i = 0; i < 4; i++){
-        if(x[i].checked == true) {
-            total += parseFloat(y[i]);
-        }
-    }
-
-    x = document.getElementById('total');
-    x.value = "$" + total;
-    
 }
